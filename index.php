709b362f91e8a5d8c1742b443ffaaeac2ae2ddf5
Albert Bytyçi <albertbytyci@gmail.com>
2017-02-08 12:33:41 +0100

    Featured Products

--- index.php
+++ index.php
@@ -7,129 +7,33 @@
 <!-- body -->
 
 <!-- include header banner-->
-<?php include 'include/header.php';  include 'include/banner.php'; ?>
+<?php include 'include/header.php';  include 'include/banner.php'; 
+		$sql = "SELECT * FROM products WHERE featured = 1";
+		$featuredp = $db->query($sql);
+?>
 <!--//header banner-->
 
 <!--content-->
 <div class="content">
 	<div class="container">
 		<div class="content-top">
-			<h1>Recent Products</h1>
+			<h1>Featured Products</h1>
 			<div class="content-top1">
-				<div class="col-md-3 col-md2">
-					<div class="col-md1 simpleCart_shelfItem">						
-							<a><img class="img-responsive same" src="images/pi.png" alt="" data-toggle="modal" data-target="#details-1"/></a>					
-						<h3>Macbook Pro</h3>
-						<div class="price">
-								<h5 class="item_price">$900</h5>
-								<a href="#" class="item_add"  data-toggle="modal" data-target="#details-1">More details</a>
-								<div class="clearfix"> </div>
+				<?php while ($product = mysqli_fetch_assoc($featuredp)): ?>
+					<div class="col-md-3 col-md2">
+						<div class="col-md1 simpleCart_shelfItem">						
+								<a><img class="img-responsive same" src="<?php echo $product['image']; ?>" alt="<?= $product['title']; ?>" data-toggle="modal" data-target="#details-1"/></a>					
+							<h3><?= $product['title']; ?></h3>
+							<div class="price">
+									<h5 class="item_price">$<?= $product['price']; ?></h5>
+									<a href="#" class="item_add"  data-toggle="modal" data-target="#details-1">More details</a>
+									<div class="clearfix"> </div>
+							</div>
 						</div>
 					</div>
-				</div>	
-			<div class="col-md-3 col-md2">
-					<div class="col-md1 simpleCart_shelfItem">
-						<a href="single.php">
-							<img class="img-responsive same" src="images/pi2.png" alt="" />
-						</a>						
-						<h3><a href="single.php">Surface Book</a></h3>
-						<div class="price">
-								<h5 class="item_price">$900</h5>
-								<a href="#" class="item_add">Add To Cart</a>
-								<div class="clearfix"> </div>
-						</div>
-						
-					</div>
-				</div>	
-			<div class="col-md-3 col-md2">
-					<div class="col-md1 simpleCart_shelfItem">
-						<a href="single.php">
-							<img class="img-responsive same" src="images/pi4.png" alt="" />
-						</a>
-						<h3><a href="single.php">Surface Studio</a></h3>
-						<div class="price">
-								<h5 class="item_price">$1000</h5>
-								<a href="#" class="item_add">Add To Cart</a>
-								<div class="clearfix"> </div>
-						</div>
-						
-					</div>
-				</div>	
-			<div class="col-md-3 col-md2">
-					<div class="col-md1 simpleCart_shelfItem">
-						<a href="single.php">
-							<img class="img-responsive same" src="images/pi1.png" alt="" />
-						</a>
-						<h3><a href="single.php">iMac</a></h3>
-						<div class="price">
-								<h5 class="item_price">$1000</h5>
-								<a href="#" class="item_add">Add To Cart</a>
-								<div class="clearfix"> </div>
-						</div>
-						
-					</div>
-				</div>	
-			<div class="clearfix"> </div>
-			</div>	
-			<div class="content-top1">
-				<div class="col-md-3 col-md2">
-					<div class="col-md1 simpleCart_shelfItem">
-						<a href="single.php">
-							<img class="img-responsive same" src="images/pi3.png" alt="" />
-						</a>
-						<h3><a href="single.php">Laptop</a></h3>
-						<div class="price">
-								<h5 class="item_price">$300</h5>
-								<a href="#" class="item_add">Add To Cart</a>
-								<div class="clearfix"> </div>
-						</div>
-						
-					</div>
-				</div>	
-			<div class="col-md-3 col-md2">
-					<div class="col-md1 simpleCart_shelfItem">
-						<a href="single.php">
-							<img class="img-responsive same" src="images/pi5.png" alt="" />
-						</a>
-						<h3><a href="single.php">Laptop</a></h3>
-						<div class="price">
-								<h5 class="item_price">$300</h5>
-								<a href="#" class="item_add">Add To Cart</a>
-								<div class="clearfix"> </div>
-						</div>
-						
-					</div>
-				</div>	
-			<div class="col-md-3 col-md2">
-					<div class="col-md1 simpleCart_shelfItem">
-						<a href="single.php">
-							<img class="img-responsive same" src="images/pi6.png" alt="" />
-						</a>
-						<h3><a href="single.php">Laptop</a></h3>
-						<div class="price">
-								<h5 class="item_price">$300</h5>
-								<a href="#" class="item_add">Add To Cart</a>
-								<div class="clearfix"> </div>
-						</div>
-						
-					</div>
-				</div>	
-			<div class="col-md-3 col-md2">
-					<div class="col-md1 simpleCart_shelfItem">
-						<a href="single.php">
-							<img class="img-responsive same" src="images/pi7.png" alt="" />
-						</a>
-						<h3><a href="single.php">Laptop</a></h3>
-						<div class="price">
-								<h5 class="item_price">$300</h5>
-								<a href="#" class="item_add">Add To Cart</a>
-								<div class="clearfix"> </div>
-						</div>
-						
-					</div>
-				</div>	
-			<div class="clearfix"> </div>
-			</div>	
+				<?php endwhile; ?>					
+				<div class="clearfix"> </div>
+			</div>				
 		</div>
 	</div>
 </div>
